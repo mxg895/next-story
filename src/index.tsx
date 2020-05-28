61ea7c9a5ae12e8a6c1a5c772e6fc1b8c209cf1c
MayKikuchi <m.may.kikuchi@gmail.com>
2020-05-28 01:29:33 -0700

    set up redux dev tool extension and install styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,24 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { theme } from './styles/theme';
+import { createStore } from 'redux';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+    // add root reducer here,
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
+
+const { Provider } = require('react-redux');
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+      <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
