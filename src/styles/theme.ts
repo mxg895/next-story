7ba848bcdc72078d3440ddd504431f2d135dd946
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-06-02 14:05:30 -0700

    Home and card (#10)
    * card and card carousel
    
    * card and card carousel

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,3 +1,15 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
-export const theme = createMuiTheme({});
+export const theme = createMuiTheme({
+    typography: {
+        h1: {
+            fontSize: 32
+        },
+        h2: {
+            fontSize: 28
+        },
+        h3: {
+            fontSize: 24
+        }
+    }
+});
