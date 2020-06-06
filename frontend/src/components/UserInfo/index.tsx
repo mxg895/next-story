1b261139a23a09a23886e99ef21f5281b00ffa0e
Michelle Gu <michelle.gu@hotmail.com>
2020-06-06 09:51:25 -0700

    add left side of profile page

---
+++ frontend/src/components/UserInfo/index.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import React from 'react';
+
+const UserInfoContainer = styled.div`
+  min-width: 300px;
+  max-width: 400px;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+  & > * {
+    margin-bottom: 10px;
+  }
+`;
+
+const ProfilePicPlaceholder = styled(AccountBoxIcon)`
+  &.MuiSvgIcon-root {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const PhotoContainer = styled.div`
+  /* margin-bottom: 5px; */
+  width: 150px;
+  /* height: 150px; */
+  &>:first-child {
+    border: solid 2px #D3D3D3;
+    height: 150px;
+  }
+`;
+
+const StyledSectionHeader = styled(Typography)`
+  &.MuiTypography-body1 {
+    font-weight: bold;
+  }
+`;
+
+const UserSummary = styled.div``;
+
+const UserInfo: React.FC<{}> = () => {
+  return (
+    <UserInfoContainer id='userinfo'>
+      <PhotoContainer id='profile-pic'>
+        <div>
+          <ProfilePicPlaceholder color='disabled' fontSize='large'/>
+        </div>
+        <Typography align='center' color='primary' variant='body1'>(Edit Profile)</Typography>
+      </PhotoContainer>
+      <UserSummary>
+        <Typography variant='body2'>User Name: Name Here</Typography>
+        <Typography variant='body2'>Books Read: 20</Typography>
+        <Typography variant='body2'>Movies Watched 10</Typography>
+      </UserSummary>
+      <div>
+        <StyledSectionHeader>About Me/Message User</StyledSectionHeader>
+        <Typography variant='body2'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Quis auctor elit sed vulputate. Lobortis elementum nibh tellus molestie nunc non blandit massa. Aliquam purus sit amet luctus venenatis lectus magna. Ac felis donec et odio pellentesque. Etiam erat velit scelerisque in dictum. Ridiculus mus mauris vitae ultricies leo integer malesuada. Sed pulvinar proin gravida hendrerit lectus. Faucibus interdum posuere lorem ipsum dolor.
+        </Typography>
+      </div>
+      <div>
+        <StyledSectionHeader>Contact</StyledSectionHeader>
+        <Typography variant='body2'>Contact Placeholder</Typography>
+      </div>
+    </UserInfoContainer>
+  );
+};
+
+export default UserInfo;
--- frontend/src/containers/Profile/index.tsx
+++ frontend/src/containers/Profile/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import styled from 'styled-components';
+import UserInfo from '../../components/UserInfo';
+
+const StyledContainer = styled(Container)`
+  padding-top: 80px;
+`;
+
+const ProfileSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const UserActivityContainer = styled.div`
+  min-width: 300px;
+`;
 
 const Profile: React.FC = () => {
   return (
-    <Container maxWidth='md'>
-      <Typography variant='h1'>Profile Page</Typography>
-    </Container>
+    <StyledContainer maxWidth='lg'>
+      {/* <Typography variant='h1'>Profile Page</Typography> */}
+      {/* <Grid spacing={1} container> */}
+      <ProfileSection>
+        <UserInfo />
+        <UserActivityContainer>user activity</UserActivityContainer>
+      </ProfileSection>
+
+        {/* <Grid item sm={3} md={6}>
+          <div>left side</div>
+        </Grid> */}
+        {/* <Grid item xs={12} sm={6} md={6}>
+          <div>Right side</div>
+        </Grid> */}
+      {/* </Grid> */}
+    </StyledContainer>
   );
 };
 
