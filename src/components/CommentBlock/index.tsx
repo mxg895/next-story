92d5de02e80274d6bc3fc20d4fbde50973bf7acd
MayKikuchi <m.may.kikuchi@gmail.com>
2020-06-17 15:48:33 -0700

    add review components

---
+++ src/components/CommentBlock/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CommentBlockProps {
+    userId: string,
+}
+const CommentBlock: React.FC<CommentBlockProps> = (props: CommentBlockProps) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default CommentBlock;
---
+++ src/components/ReviewList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface ReviewListProps {
+    mediaId: string,
+}
+
+const ReviewList: React.FC<ReviewListProps> = (props: ReviewListProps) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default ReviewList;
