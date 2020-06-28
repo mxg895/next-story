70889999a082027f12d8cf3339f340ebec07d197
MayKikuchi <m.may.kikuchi@gmail.com>
2020-06-27 18:51:55 -0700

    remove redux from media modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import Routes from './routes';
-import MediaModal from './components/MediaModal';
-import { connect } from 'react-redux';
 import NavigationBar from './components/NavigationBar/index';
 
 
-function App(props: any) {
-    const { mediaModalObject } = props;
-
+function App() {
     return (
         <div>
-            {mediaModalObject &&
-                <MediaModal
-                    isOpen={mediaModalObject.isModalOpen}
-                    modalData={mediaModalObject.data}
-                />}
           <NavigationBar/>
           <Routes/>
         </div>
     );
 }
 
-const mapStateToProps = (state: any) => {
-    return {
-        mediaModalObject: state.mediaModalReducer.mediaModalObject
-    };
-};
 
-export default connect(mapStateToProps)(App);
+export default App;
