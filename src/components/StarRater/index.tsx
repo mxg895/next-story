a5c9841f95f48f098f101687fe78d6d23892d227
MayKikuchi <m.may.kikuchi@gmail.com>
2020-06-27 23:58:40 -0700

    make StarRater component

---
+++ src/components/StarRater/index.tsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import {IconButton} from '@material-ui/core';
+import StarIcon from '@material-ui/icons/Star';
+import styled from 'styled-components';
+
+interface StarRaterProps {
+    isClickable: boolean;
+    avgRating?: number;
+}
+
+const StyledIconButton = styled(IconButton)`
+    padding: 0px !important;
+`;
+
+const StarToolTip = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const StarRater: React.FC<StarRaterProps> = (props: StarRaterProps) => {
+    const [ratedStar, setStar] = useState(0);
+    const { isClickable, avgRating } = props;
+    let twoDecimalRating: number = 0;
+    let closestWholeNumber: number;
+    if (avgRating) {
+        const twoDecimalRatingString = avgRating.toFixed(2);
+        twoDecimalRating = Number(twoDecimalRatingString);
+        closestWholeNumber = Math.round(avgRating);
+    }
+
+    function clickStar(starIndex: number) {
+        setStar(starIndex);
+        // TODO use the api to set the rating (add the media type and media id as props)
+    }
+
+    return (
+        <StarToolTip>
+            {[...Array(5)].map((star, index) => {
+                return (
+                    <>
+                        {isClickable ?
+                            <StyledIconButton key={index} size={'small'} onClick={() => clickStar(index + 1)}>
+                                {index < ratedStar ?
+                                    <StarIcon color={'primary'} />
+                                    :
+                                    <StarBorderIcon color={'primary'} />
+                                }
+                            </StyledIconButton>
+                            :
+                            <>
+                                {index <  closestWholeNumber ?
+                                    <StarIcon color={'primary'} fontSize={'small'}/>
+                                    :
+                                    <StarBorderIcon color={'primary'} fontSize={'small'}/>
+                                }
+                            </>
+                        }
+                    </>
+                );
+            })}
+        </StarToolTip>
+    );
+};
+
+export default StarRater;
--- src/containers/MediaPage/index.tsx
+++ src/containers/MediaPage/index.tsx
@@ -5,17 +5,36 @@ import ReviewList from '../../components/ReviewList';
 import { MediaType } from '../../constants/dataTypes';
 import MockCover from '../../assets/MockCover.png';
 import styled from 'styled-components';
-import { Grid } from '@material-ui/core';
+import {Box, Grid} from '@material-ui/core';
 import TagsSection from '../../components/TagsSection';
+import StarRater from '../../components/StarRater';
 
 const StyledImage = styled.img`
     width: 100%;
     max-width: 300px;
+    margin-bottom: 15px;
+`;
+
+const StyledGridItem = styled(Grid)`
+    text-align: center;
+`;
+
+const VerticallyCenteredDiv = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+const CenteredDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 10px;
 `;
 
 const MediaPage: React.FC<{}> = (props: any ) => {
     const { id, mediaType } = props.match.params;
-    const { title, image, people, blurb, tags, nextStoryTags } = useMemo(() => {
+    const { title, image, people, blurb, tags, nextStoryTags, avgRating } = useMemo(() => {
         // TODO get the media info from an api call using the media id
         return {
             title: 'Mock Title Harry Potter',
@@ -26,19 +45,31 @@ const MediaPage: React.FC<{}> = (props: any ) => {
             tags: ['fantasy', 'action', 'sci-fi', 'superheroes', 'tag1', 'tag2', 'tag3'],
             nextStoryTags: ['cliffhangers', 'sad ending'],
             blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            avgRating: 3
+            avgRating: 3.231351513
         };
     }, [id]);
-// todo: make grid a column on smaller screens
+
     return (
         <Container maxWidth='lg'>
             <Grid container direction={'row'}  spacing={5}>
-                <Grid item sm={3}>
+                <StyledGridItem item sm={3}>
                     <StyledImage src={image} />
-                </Grid>
+                    <div>
+                        Your rating:
+                        <CenteredDiv>
+                            <StarRater isClickable={true} avgRating={avgRating}/>
+                        </CenteredDiv>
+                    </div>
+                </StyledGridItem>
                 <Grid item sm={6}>
-                    <Typography variant='h1' gutterBottom>{title}</Typography>
-                    <Typography variant='subtitle1' gutterBottom>{people}</Typography>
+                    <Typography variant='h1'>{title}</Typography>
+                    <VerticallyCenteredDiv>
+                        {'Avg rating: '}
+                        <StarRater isClickable={false} avgRating={avgRating}/>
+                    </VerticallyCenteredDiv>
+                    <Typography variant='subtitle1' gutterBottom>
+                        <Box fontStyle='italic'>{people}</Box>
+                    </Typography>
                     <Typography variant='body1'>{blurb}</Typography>
                 </Grid>
                 <Grid item sm={3}>
