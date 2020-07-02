4ce0960592407d695e3177b37e62ef475c5277ad
MayKikuchi <m.may.kikuchi@gmail.com>
2020-07-02 00:44:00 -0700

    use Mui Rating component instead of custom star rating

--- src/containers/MediaPage/index.tsx
+++ src/containers/MediaPage/index.tsx
@@ -57,12 +57,17 @@ const MediaPage: React.FC<{}> = (props: any ) => {
             tags: ['fantasy', 'action', 'sci-fi', 'superheroes', 'tag1', 'tag2', 'tag3'],
             nextStoryTags: ['cliffhangers', 'sad ending'],
             blurb: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            avgRating: 3.231351513
+            avgRating: 3.531351513
         };
     }, [id]);
 
     useEffect(() => {
         // todo get if this media is on the user's watch/read later list and set the state
+    }, [id]);
+
+    const userRating = useMemo(() => {
+        // TODO get the user's rating from an api call using the media id and user id
+        return 4;
     }, [id]);
 
     const addOrRemoveWatchReadLater = (mediaType: MediaType, mediaId: string) => {
@@ -85,7 +90,7 @@ const MediaPage: React.FC<{}> = (props: any ) => {
                         <div>
                             Your rating:
                             <CenteredDiv>
-                                <StarRater />
+                                <StarRater userRating={userRating}/>
                             </CenteredDiv>
                             <WatchReadButton onClick={() => addOrRemoveWatchReadLater(mediaType, id)} isForLater={isForLater}>
                                 {`${isForLater ? 'Remove from' : 'Add to'} ${mediaType === MediaType.movie ? 'watch' : 'read'} later`}
