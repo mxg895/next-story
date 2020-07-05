87f292a7066c58eecdd21b31acba999cdf45d97a
Warren <faaigame@gmail.com>
2020-07-05 12:21:06 -0700

    delete commented out

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -54,34 +54,4 @@ router.put('/:mediaType/:mediaId/:userId', (req, res) => {
     }
 });
 
-
-// router.delete('/:mediaType/:mediaId/:userId', (req, res) => {
-//     const { mediaId, mediaType, userId } = req.params;
-//     ReviewRating.deleteOne({ mediaType: mediaType, id: mediaId, userId: userId })
-//         .then(result => {
-//             console.log('Success. Deleted reviewRating document, count:', result.deletedCount);
-//             res.status(200).json(result);
-//         })
-//         .catch((err) => {
-//             console.log('Error deleting reviewRating document: ', err);
-//             res.status(500);
-//         });
-// });
-
-// router.put('/rating', (req, res) => {
-//     const { rating, userId, mediaId, mediaType } = req.body;
-//     ReviewRating.findOneAndUpdate({ mediaType: mediaType, id: mediaId, userId: userId },
-//         { mediaType: mediaType, userId: userId, id: mediaId, rating: rating },
-//         { new: true, upsert: true })
-//         .then(reviewRating => {
-//             console.log('Success. Updated or posted rating: ', reviewRating);
-//             res.status(200).json(reviewRating);
-//         })
-//         .catch((err) => {
-//             console.log('Error upserting review: ', err);
-//             res.status(500);
-//         });
-// });
-
-
 module.exports = router;
