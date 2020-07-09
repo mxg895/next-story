6e653ac02b0015f6239e8625ecf4933f578653d2
MayKikuchi <m.may.kikuchi@gmail.com>
2020-07-09 02:03:08 -0700

    google books looks like the way to go... doesn't have popular though

--- backend/routes/thridPartyBookApi.js
+++ backend/routes/thridPartyBookApi.js
@@ -1,44 +1,35 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
-const goodreadsBaseUrl = 'https://www.goodreads.com/';
-const goodreadsApiKey = process.env.GOODREADS_API_KEY;
-const openlibraryBaseUrl = 'https://openlibrary.org/';
-var parseString = require('xml2js').parseString;
 
-router.get('/goodreads/isbn/:isbn', (req, res) => {
-    const isbn = req.params.isbn;
-    axios.get(`${goodreadsBaseUrl}/search/index.xml`,
-        { params: { key: goodreadsApiKey, q: isbn } })
-        .then((response) => {
-            parseString(response.data, function (err, result) {
-                const stringifiedResult = JSON.stringify(result);
-                const parsedResult = JSON.parse(stringifiedResult);
-                console.log('Succeeded getting book from goodreads by isbn');
-                res.status(200).json(parsedResult);
-            });
-        })
-        .catch((error) => console.log(error));
-});
-
-// spaces should be replaced with %20, can maybe parse to url string using external library?
-router.get('/openLibrary/title/:title', (req, res) => {
-    const bookTitle = req.params.title;
-    axios.get(`${openlibraryBaseUrl}/search.json?title=${bookTitle}`)
-        .then((response) => {
-            console.log('Succeeded getting book from openLibrary:', response.data.docs[0]);
-            res.status(200);
-        })
-        .catch((error) => console.log(error));
-});
-
-// https://www.googleapis.com/books/v1/volumes?q=isbn:9780439554930
+// note gets first 10 matches ... query can be title, id, genre, etc (basically anything in the 'response' data)
 router.get('/googleBooks/searchOne/:query', (req, res) => {
     const bookQuery = req.params.query;
     axios.get(` https://www.googleapis.com/books/v1/volumes?q=${bookQuery}`)
         .then((response) => {
-            console.log('Succeeded getting book from openLibrary:', response.data.items[0]);
-            res.status(200);
+            console.log(response);
+            const numberFound = response.data.totalItems;
+            const numberToGet = numberFound >= 10 ? 10 : numberFound;
+            const returnList = [];
+            let i = 0;
+            while (i < numberToGet) {
+                const foundItem = response.data.items[i];
+                const returnObject = {
+                    id: foundItem.id,
+                    title: foundItem.volumeInfo && foundItem.volumeInfo.title,
+                    mediaType: 'book',
+                    image: foundItem.volumeInfo && foundItem.volumeInfo.imageLinks
+                        && foundItem.volumeInfo.imageLinks.thumbnail,
+                    genres: foundItem.volumeInfo && foundItem.volumeInfo.categories,
+                    blurb: foundItem.volumeInfo && foundItem.volumeInfo.description,
+                    people: foundItem.volumeInfo && foundItem.volumeInfo.authors,
+                    publishedDate: foundItem.volumeInfo && foundItem.volumeInfo.publishedDate
+                }
+                returnList.push(returnObject);
+                i++;
+            }
+            console.log('Succeeded getting book from openLibrary:', returnList);
+            res.status(200).json(returnList);
         })
         .catch((error) => console.log(error));
 });
