9b3b1444a8617c0655fcaf6735a07af39319e7f1
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-07-09 23:43:37 -0700

    Hook up book api (#73)
    * examples for 3 different book apis
    
    * google books looks like the way to go... doesn't have popular though
    
    * serchOne -> searchTen
    
    * implement searchOne

---
+++ backend/routes/thridPartyBookApi.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+
+// note gets first 10 matches ... query can be title, id, genre, etc (basically anything in the 'response' data)
+router.get('/googleBooks/searchTen/:query', (req, res) => {
+    const bookQuery = req.params.query;
+    axios.get(` https://www.googleapis.com/books/v1/volumes?q=${bookQuery}`)
+        .then((response) => {
+            console.log(response);
+            const numberFound = response.data.totalItems;
+            const numberToGet = numberFound >= 10 ? 10 : numberFound;
+            const returnList = [];
+            let i = 0;
+            while (i < numberToGet) {
+                const foundItem = response.data.items[i];
+                const returnObject = {
+                    id: foundItem.id,
+                    title: foundItem.volumeInfo && foundItem.volumeInfo.title,
+                    mediaType: 'book',
+                    image: foundItem.volumeInfo && foundItem.volumeInfo.imageLinks
+                        && foundItem.volumeInfo.imageLinks.thumbnail,
+                    genres: foundItem.volumeInfo && foundItem.volumeInfo.categories,
+                    blurb: foundItem.volumeInfo && foundItem.volumeInfo.description,
+                    people: foundItem.volumeInfo && foundItem.volumeInfo.authors,
+                    publishedDate: foundItem.volumeInfo && foundItem.volumeInfo.publishedDate
+                }
+                returnList.push(returnObject);
+                i++;
+            }
+            console.log('Succeeded getting book from googleBooks:', returnList);
+            res.status(200).json(returnList);
+        })
+        .catch((error) => console.log(error));
+});
+
+// note gets first match ...usually only reliable when querying by id
+router.get('/googleBooks/searchOne/:query', (req, res) => {
+    const bookQuery = req.params.query;
+    axios.get(` https://www.googleapis.com/books/v1/volumes?q=${bookQuery}`)
+        .then((response) => {
+            console.log(response);
+            const foundItem = response.data.items[0];
+            const returnObject = {
+                id: foundItem.id,
+                title: foundItem.volumeInfo && foundItem.volumeInfo.title,
+                mediaType: 'book',
+                image: foundItem.volumeInfo && foundItem.volumeInfo.imageLinks
+                    && foundItem.volumeInfo.imageLinks.thumbnail,
+                genres: foundItem.volumeInfo && foundItem.volumeInfo.categories,
+                blurb: foundItem.volumeInfo && foundItem.volumeInfo.description,
+                people: foundItem.volumeInfo && foundItem.volumeInfo.authors,
+                publishedDate: foundItem.volumeInfo && foundItem.volumeInfo.publishedDate
+            }
+            console.log('Succeeded getting book from googleBooks:', returnObject);
+            res.status(200).json(returnObject);
+        })
+        .catch((error) => console.log(error));
+});
+
+module.exports = router;
