8adc51f50e7bd973496b2eadf609e9b1f6574afa
MayKikuchi <m.may.kikuchi@gmail.com>
2020-07-28 01:09:09 -0700

    implement query by tag

--- frontend/src/containers/SingleSearchResultPage/index.tsx
+++ frontend/src/containers/SingleSearchResultPage/index.tsx
@@ -20,8 +20,6 @@ const SingleSearchResultPage: React.FC = (props: any) => {
 
     const [queryStartIndex, setQueryStartIndex] = useState(0);
 
-    const [mongoMoviesMatchingTag, setMongoMoviesMatchingTag] = useState<Array<any>>([]);
-    const [mongoBooksMatchingTag, setMongoBooksMatchingTag] = useState<Array<any>>([]);
     const [movieResults, setMovieResults] = useState<Array<any>>([]);
     const [bookResults, setBookResults] = useState<Array<any>>([]);
 
@@ -37,14 +35,12 @@ const SingleSearchResultPage: React.FC = (props: any) => {
         try {
             const mongoMovieRes = await axios.get(`/movies/withTag/${query}`);
             mongoMovies = mongoMovieRes.data;
-            setMongoMoviesMatchingTag(mongoMovies);
         } catch (e) {
             console.log('error fetching movies from mongo for tag', e);
         }
         try {
             const mongoBookRes = await axios.get(`/books/withTag/${query}`);
             mongoBooks = mongoBookRes.data;
-            setMongoBooksMatchingTag(mongoBooks);
         } catch (e) {
             console.log('error fetching movies from mongo for tag', e);
         }
@@ -55,35 +51,46 @@ const SingleSearchResultPage: React.FC = (props: any) => {
         const bookData: any[] = [];
         const movieData: any[] = [];
         const maxEndIndex = queryStartIndex + increaseIndexBy;
-        const movieEndIndex = mongoBooks.length >= maxEndIndex ? maxEndIndex : mongoBooks.length;
+        const movieEndIndex = mongoMovies.length >= maxEndIndex ? maxEndIndex : mongoMovies.length;
+        const bookEndIndex = mongoBooks.length >= maxEndIndex ? maxEndIndex : mongoBooks.length;
         for(let i = queryStartIndex; i < movieEndIndex; i++){
             const movieId = mongoMovies[i].movieId;
-            console.log(movieId);
             try {
                 const movieRes = await axios.get(`/thirdPartyMovieApi/tmdbMovies/searchOneById/${movieId}`);
-                movieData.push(movieRes.data);
-                console.log(movieRes.data);
-                setAllResults((allResults) => [...allResults, movieRes.data]);
+                let movieWithNextStoryTags = movieRes.data;
+                const thisMongoMovieMaybe = mongoMovies.filter((m) => m.movieId === movieId);
+                const thisMongoMovie = thisMongoMovieMaybe.length > 0 && thisMongoMovieMaybe[0];
+                if (thisMongoMovie) {
+                    movieWithNextStoryTags.nextStoryTags = thisMongoMovie.nextStoryTags;
+                } else {
+                    movieWithNextStoryTags.nextStoryTags = [];
+                }
+                movieData.push(movieWithNextStoryTags);
+                setAllResults((allResults) => [...allResults, movieWithNextStoryTags]);
+                setMovieResults((movieResults) => [...movieResults, movieWithNextStoryTags]);
             } catch (e) {
                 console.log(e);
             }
         }
-        // const bookEndIndex = mongoBooks.length >= maxEndIndex ? maxEndIndex : mongoBooks.length;
-        // console.log('mongoBooks', mongoBooks);
-        // console.log('maxEndIndex', maxEndIndex);
-        // console.log('bookEndIndex', bookEndIndex);
-        // console.log('queryStartIndex', queryStartIndex);
-        // for(let i = queryStartIndex; i < bookEndIndex; i++){
-        //     const bookId = mongoBooks[i].bookId;
-        //     // console.log(bookId);
-        //     try {
-        //         const bookRes = await axios.get(`/thirdPartyBookApi/googleBooks/searchOneById/${bookId}`);
-        //         bookData.push(bookRes.data);
-        //         setAllResults((allResults) => [...allResults, bookRes.data]);
-        //     } catch (e) {
-        //         console.log(e);
-        //     }
-        // }
+        for(let i = queryStartIndex; i < bookEndIndex; i++){
+            const bookId = mongoBooks[i].bookId;
+            try {
+                const bookRes = await axios.get(`/thirdPartyBookApi/googleBooks/searchOneById/${bookId}`);
+                let bookWithNextStoryTags = bookRes.data;
+                const thisMongoBookMaybe = mongoBooks.filter((b) => b.bookId === bookId);
+                const thisMongoBook = thisMongoBookMaybe.length > 0 && thisMongoBookMaybe[0];
+                if (thisMongoBook) {
+                    bookWithNextStoryTags.nextStoryTags = thisMongoBook.nextStoryTags;
+                } else {
+                    bookWithNextStoryTags.nextStoryTags = [];
+                }
+                bookData.push(bookWithNextStoryTags);
+                setAllResults((allResults) => [...allResults, bookWithNextStoryTags]);
+                setBookResults((bookResults) => [...bookResults, bookWithNextStoryTags]);
+            } catch (e) {
+                console.log(e);
+            }
+        }
         if (movieData.length === 0) {
             setHasMoreMovieResults(false);
         }
@@ -93,19 +100,17 @@ const SingleSearchResultPage: React.FC = (props: any) => {
         return [movieData, bookData];
     }
 
-    // todo: pull out get media tags so that it doesn't have to fetch each time more is revealed with infinite scroll
     useEffect(() => {
         switch (queryType) {
             case SingleQueryType.tag:
                 getMediaForTag().then((res) => {
                     const [mongoMovies, mongoBooks] = res;
-                    console.log(res);
+                    console.log('mongo movies and books:', res);
                     getFromThirdParty(mongoMovies, mongoBooks).then((res) => {
                         console.log(res);
                         const [movieData, bookData] = res;
                         setMovieResults(movieData);
                         setBookResults(bookData);
-                        // setAllResults((allResults) => [...allResults, ...movieData, ...bookData]);
                     }).catch((e) => console.log(e));
                 }).catch((e) => console.log(e));
                 break;
