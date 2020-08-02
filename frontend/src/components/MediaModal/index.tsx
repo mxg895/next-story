96aeb69bf0034e59518d7e7f637714ef51acb074
MayKikuchi <48613270+MayKikuchi@users.noreply.github.com>
2020-08-01 17:46:41 -0700

    refactor media modal with dialog instead of modal component (#107)

--- frontend/src/components/MediaModal/index.tsx
+++ frontend/src/components/MediaModal/index.tsx
@@ -1,5 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {Modal, useMediaQuery, Typography, Paper, IconButton} from '@material-ui/core';
+import {
+    Box,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Grid,
+    useMediaQuery,
+    Typography,
+    IconButton
+} from '@material-ui/core';
 import styled from 'styled-components';
 import CloseIcon from '@material-ui/icons/Close';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
@@ -16,70 +26,6 @@ interface MediaModalProps {
     modalData: CardData,
     setModalOpen: (open: boolean) => void
 }
-
-const CenteredBody = styled.div<{ isSmall: boolean, isShort: boolean }>`
-    width: ${(props) => props.isSmall || props.isShort ? '100vw' : '500px'};
-    height: ${(props) => props.isSmall || props.isShort ? '100vh' : '500px'};
-    position: absolute;
-    left: 50% !important;
-    top: 50% !important;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    position: relative;
-`;
-
-const ModalHeader = styled.div`
-    background-color: ${({ theme }) => theme.palette.primary.main};
-    height: 50px;
-    width: 100%;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-`;
-
-const ModalContent = styled.div<{ isSmall: boolean, isShort: boolean }>`
-    height: ${(props) => props.isSmall || props.isShort ? '100%' : '450px'};
-    overflow: auto;
-`;
-
-const TopContainer = styled.div<{ isShort: boolean }>`
-    max-height: ${(props) => props.isShort ? '80%' : '350px'};
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    margin: 10px;
-    overflow: auto;
-`;
-
-const MediaImage = styled.img<{ isShort: boolean }>`
-    max-width: ${(props) => props.isShort ? '30%' : '50%'};
-`;
-
-const TopLeftContainer = styled.div<{ isShort: boolean }>`
-    max-height: ${(props) => props.isShort ? '80%' : '350px'};
-    display: flex;
-    flex-direction: column;
-    margin-right: 10px;
-    margin-left: 10px;
-`;
-
-const MediaTags = styled.div`
-    overflow: auto;
-`;
-
-const MediaBlurb = styled.div<{ isSmall: boolean, isShort: boolean }>`
-    margin: 10px;
-    margin-bottom: ${(props) => props.isSmall || props.isShort ? '100px' : '35px'};
-`;
-
-const ModalFooter = styled(Paper)`
-    height: 35px;
-    width: 100%;
-    position:absolute;
-    bottom:0;
-    display: flex;
-    justify-content: flex-end;
-`;
 
 const StyledButton = styled(Button)`
     margin-right: 10px;
@@ -98,6 +44,27 @@ const StyledCloseIcon = styled(CloseIcon)`
     color: white;
     &:hover {
         color: grey;
+    }
+`;
+
+const StyledDialogTitle = styled(DialogTitle)`
+    background-color: ${({ theme }) => theme.palette.primary.main};
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    &.MuiDialogTitle-root {
+        margin-bottom: 10px;
+        height: 25px;
+    }
+`;
+
+const StyledImage = styled.img`
+    width: 100%;
+`;
+
+const StyledDialogActions = styled(DialogActions)`
+    &.MuiDialogActions-root {
+        box-shadow: 10px 10px 5px grey;
     }
 `;
 
@@ -147,50 +114,65 @@ const MediaModal: React.FC<MediaModalProps> = (props: MediaModalProps) => {
     }
 
     return (
-        <Modal open={isOpen} onClose={() => setModalOpen(false)}>
-            <CenteredBody isSmall={isSmall} isShort={isShort}>
-                <ModalHeader>
-                    <IconButton onClick={() => setModalOpen(false)}>
-                        <StyledCloseIcon />
-                    </IconButton>
-                </ModalHeader>
-                <ModalContent isSmall={isSmall} isShort={isShort}>
-                    <TopContainer isShort={isShort}>
-                        <MediaImage src={modalData?.image} isShort={isShort} />
-                        <TopLeftContainer  isShort={isShort}>
-                            <Typography variant='h3' gutterBottom>{modalData?.title} {modalData?.mediaType === MediaType.movie ? '(Movie)' : '(Book)'}</Typography>
-                            <Typography variant='subtitle2' >Avg members rating: {!averageRating && 'No rating'}</Typography>
+        <Dialog
+            scroll={'paper'}
+            open={isOpen}
+            onClose={() => setModalOpen(false)}
+            fullScreen={isSmall || isShort}
+        >
+            <StyledDialogTitle disableTypography>
+                <IconButton onClick={() => setModalOpen(false)}>
+                    <StyledCloseIcon />
+                </IconButton>
+            </StyledDialogTitle>
+            <DialogContent>
+                <Grid container direction={'column'} spacing={2}>
+                    <Grid container direction={'row'} spacing={2}>
+                        <Grid item sm={4} style={{ width: '100%' }}>
+                            <Box display={'flex'} justifyContent={'center'} alignItems={'center'} >
+                                <StyledImage src={modalData?.image}/>
+                            </Box>
+                        </Grid>
+                        <Grid item sm={8}>
+                            <Typography variant='h3' gutterBottom>
+                                {modalData?.title} {modalData?.mediaType === MediaType.movie ? '(Movie)' : '(Book)'}
+                            </Typography>
+                            <Typography variant='subtitle2' >
+                                Avg members rating: {!averageRating && 'No rating'}
+                            </Typography>
                             {averageRating > 0 && <StarRater readOnlyRating={averageRating} readonly />}
-                            <MediaTags>
-                                Genres:
-                                <TagsSection
-                                    tags={modalData?.genres || []}
-                                    singleQueryType={SingleQueryType.genre}
-                                />
-                                {storyTags.length > 0 && <>
+                            Genres:
+                            <TagsSection
+                                tags={modalData?.genres || []}
+                                singleQueryType={SingleQueryType.genre}
+                            />
+                            {storyTags.length > 0 &&
+                                <>
                                     Tags:
                                     <TagsSection
                                         tagObjects={storyTags || []}
                                         singleQueryType={SingleQueryType.tag}
                                     />
-                                </>}
-                            </MediaTags>
-                        </TopLeftContainer>
-                    </TopContainer>
-                    <MediaBlurb isSmall={isSmall} isShort={isShort}>
+                                </>
+                            }
+                        </Grid>
+                    </Grid>
+                    <Grid item sm={12}>
                         <Interweave content={modalData?.blurb || ''} />
-                    </MediaBlurb>
-                </ModalContent>
-                <ModalFooter elevation={3}>
-                    <StyledButton
-                        endIcon={<ArrowForwardIcon />}
-                        onClick={() => goToPage()}
-                    >
-                        Go to page
-                    </StyledButton>
-                </ModalFooter>
-            </CenteredBody>
-        </Modal>
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <Box boxShadow={3}>
+                <StyledDialogActions>
+                        <StyledButton
+                            endIcon={<ArrowForwardIcon />}
+                            onClick={() => goToPage()}
+                        >
+                            Go to page
+                        </StyledButton>
+                </StyledDialogActions>
+            </Box>
+        </Dialog>
     );
 };
 
